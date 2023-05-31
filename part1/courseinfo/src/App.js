af0bc357cb3fa396ef8027fa4218f56fa390824d
Erkka Lehtoranta <erkka.lehtoranta@gmail.com>
2023-06-01 02:20:38 +0300

    Cleaned exercise 1.2-5, ready for return

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,5 +1,3 @@
-// U fokin wot m8
-
 const App = () => {
   const course = {
     name: 'Half Stack application development',
@@ -29,7 +27,7 @@ const App = () => {
 }
 
 const Total = ({parts}) => {
-  console.log("In Total", parts);
+  // console.log("In Total", parts);
   const sum = parts.map(parts => parts.exercises).reduce((a, b) => a + b);
 
   return (
@@ -38,7 +36,7 @@ const Total = ({parts}) => {
 }
 
 const Header = ({name}) => {
-  console.log("Header", name);
+  // console.log("Header", name);
 
   return (
     <h1>{name}</h1>
@@ -46,10 +44,9 @@ const Header = ({name}) => {
 }
 
 const Content = ({parts}) => {
-  console.log("Parts", parts);
+  // console.log("Parts", parts);
 
   // Following map() works also with `name={parts.name} exercises={parts.exercises}`
-  // For all I know, this might be HORRIBLE practice, but for now no cap idc
   return (
     <div>
       {parts.map((parts, i) => <Part {...parts} key={i}/>)}
@@ -58,57 +55,10 @@ const Content = ({parts}) => {
 }
 
 const Part = ({name, exercises}) => {
-  console.log("We should have PARTS ffs", name, exercises)
+  // console.log("We should have PARTS ffs", name, exercises)
   return (
     <p>{name} {exercises}</p>
   )
 }
 
-/*
-
-const App = () => {
-  const props = {
-    name: 'Richard Hendricks',
-    occupation: 'shit CEO',
-  }
-
-  return (
-    <>
-      <Header />
-      <User {...props}/>
-      <DateNow />
-      <Footer />
-    </>
-  )}
-
-function DateNow() {
-  const date = new Date();
-  console.log(date);
-
-  return (
-    <p>The time is now {date.toLocaleTimeString()}</p>
-  )
-}
-
-function User({name, occupation}) {
-  console.log(name, occupation)
-
-  return (
-    <p>Hello {name}! You're a {occupation}!</p>
-  )
-}
-
-function Header() {
-  return (
-    <header>Wassup birdies</header>
-  )
-}
-
-function Footer() {
-  return (
-    <footer>The kids aren't alright</footer>
-  )
-}
-
-*/
 export default App;
