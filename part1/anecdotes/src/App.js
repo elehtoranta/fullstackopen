f23e978b320a53673fec31a20eea5df0c074a976
Erkka Lehtoranta <erkka.lehtoranta@gmail.com>
2023-06-03 03:07:46 +0300

    Added conditional display to most voted anecdotes
    Most voted anecdotes now only show when votes have already
    been recorded, until which 'no votes yet' is displayed.
    
    Still a bit dodgy with vote ties resulting in change or no
    change depending on the index of the anecdote, but this is
    a problem for future me.

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -15,64 +15,69 @@ const App = () => {
 
   const [selected, setSelected] = useState(0);
   const [votes, setVotes] = useState(Array(anecdotes.length).fill(0));
+  const [mostVotedIndex, setMostVotedIndex] = useState(-1);
 
-  console.log(`selected ${selected}, votes ${votes}, votes array length ${votes.length}`);
+  // console.log(`selected ${selected}, votes ${votes}, votes array length ${votes.length}`);
+  console.log(`most voted`, mostVotedIndex);
 
   function getRandomIndex() {
     let randomIndex;
 
     do {
-      // Note to self: no off-by-one, since random() produces values from 0 to 1 *exclusive*.
       randomIndex = Math.floor(Math.random() * anecdotes.length);
     } while (randomIndex === selected);
-
     setSelected(randomIndex);
   }
 
-  // Apparently this can be done inline with head/modify/tail slices, but it's really not that pretty.
+  /* Apparently this can be done inline with head/modify/tail slices,
+  * but it's really not that pretty. */
   function voteAnecdote() {
-    const copy = [...votes];
-    copy[selected] += 1;
-    setVotes(copy);
-  }
+    const votesCopy = [...votes];
 
-  /** @return String, the most voted anecdote */
-  function getMostVotedAnecdote() {
-    console.log((Math.max(...votes)));
-    return (anecdotes[votes.indexOf(Math.max(...votes))]);
+    votesCopy[selected] += 1;
+    setVotes(votesCopy);
+    // On two equally voted entries, this still sets the first index as
+    // the most voted, causing unintuitive changes on ties.
+    setMostVotedIndex(() => votesCopy.indexOf(Math.max(...votesCopy)));
   }
 
   return (
     <>
-      <div className="current-anecdote">
-        <h1>Anecdote of the day</h1>
-        <p>{anecdotes[selected]}</p>
-        <p>Has {votes[selected]} votes</p>
-      </div>
+      <h1>Anecdote of the day</h1>
+      <Anecdote
+        className="current-anecdote"
+        content={anecdotes[selected]}
+        votes={votes[selected]}
+      />
+
       <div className="controls">
-        <Button
-          value="Vote"
-          onClick={voteAnecdote}
-        />
-        <Button
-          value="Next anecdote"
-          onClick={getRandomIndex}
-        />
+        <button onClick={voteAnecdote}>Vote</button>
+        <button onClick={getRandomIndex}>Next anecdote</button>
       </div>
-      <div className="most-voted-anecdote">
-        <h1>Anecdote with most votes</h1>
-        <p>{getMostVotedAnecdote()}</p>
-      </div>
+
+      <h1>Most voted anecdote</h1>
+      <Anecdote
+        className="most-voted-anecdote"
+        content={anecdotes[mostVotedIndex]}
+        votes={votes[mostVotedIndex]}
+        render={mostVotedIndex >= 0}
+      />
     </>
   )
 }
 
-const Button = ({value, onClick}) => {
-  return (
-    <button onClick={onClick}>
-      {value}
-    </button>
-  )
+const Anecdote = ({
+  className,
+  content,
+  votes,
+  render=true
+}) => {
+  return render ? (
+    <div className={className}>
+      <p>{content}</p>
+      <p>Has {votes} votes</p>
+    </div>
+  ) : ('No votes yet')
 }
 
 export default App;
