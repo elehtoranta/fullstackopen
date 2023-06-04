28b3afcdcb9184154cfe7a462f5d7dbccc13dd54
Erkka Lehtoranta <erkka.lehtoranta@gmail.com>
2023-06-04 08:05:37 +0300

    Completed 2.4: added support for arbitrary courses

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,33 +1,55 @@
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    id: 1,
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      },
-      {
-        name: 'Redux',
-        exercises: 11,
-        id: 4
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
   return (
-    <Course course={course} />
+    <>
+      <h1>Course information</h1>
+
+      {courses.map(course => <Course course={course} key={course.id} />)}
+    </>
   )
 }
 
@@ -36,14 +58,14 @@ const Course = ({ course }) => {
 
   return (
     <div className='course'>
-      <Header name={name}/>
+      <CourseHeader name={name}/>
       <Content parts={parts}/>
     </div>
   )
 }
 
-const Header = ({ name }) =>
-  <h1>{name}</h1>
+const CourseHeader = ({ name }) =>
+  <h2>{name}</h2>
 
 const Content = ({ parts }) =>
   <div className="content">
